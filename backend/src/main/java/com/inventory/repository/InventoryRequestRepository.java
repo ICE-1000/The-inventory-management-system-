83a84b9ecff82927bcfee5370a925d5f4ad3698d
MUSONDA KENNEDY <kennedy.musonda@cs.unza.zm>
2026-06-11 12:36:37 +0200

    retrying request repository

--- backend/src/main/java/com/inventory/repository/InventoryRequestRepository.java
+++ backend/src/main/java/com/inventory/repository/InventoryRequestRepository.java
@@ -5,7 +5,7 @@ import com.inventory.model.RequestStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
-
+//was not commited to the current git commit , anywsay retry
 import java.util.List;
 import java.util.UUID;
 
